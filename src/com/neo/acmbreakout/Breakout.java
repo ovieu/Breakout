13aab65858561c86435945c6ba3305455dfc789b
Ovie Umukoro <ovie.u.umukoro@gmail.com>
2018-01-13 17:44:03 -0500

    Add game loop logic

--- src/com/neo/acmbreakout/Breakout.java
+++ src/com/neo/acmbreakout/Breakout.java
@@ -90,6 +90,10 @@ public class Breakout extends GraphicsProgram {
      */
     public void run() {
         setupGame();
+        while(true) {
+            moveBall();
+            pause(DELAY);
+        }
     }
 
     /**
